52c2a0647f67b0c45cd2d98371713fe16cd3d6f3
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-01-31 15:43:33 +0000

    Add operate() function

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ function divide(a, b) {
     let divSum = a / b;
     return divSum;
 }
+
+function operate(a, operator, b) {
+    if (operator == '+') {
+        return add(a, b);
+    } else if (operator == '-') {
+        return subtract(a, b);
+    } else if (operator == '*') {
+        return multiply(a, b);
+    } else if (operator == '/') {
+        return divide(a, b);
+    } else {
+        return 'Invalid operator';
+    }
+}
