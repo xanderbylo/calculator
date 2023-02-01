dd0533d2476cfa550e7295ca4f125c5e6e1ea968
Xander Bylo <92520776+xanderbylo@users.noreply.github.com>
2023-02-01 09:29:31 +0000

    Add event listeners to digits
    Event listeners update the numbers on display

--- index.js
+++ index.js
@@ -1,3 +1,93 @@
+let displayValue = document.querySelector('#display')
+
+let zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    if (displayValue.textContent !== '0') {
+        displayValue.textContent += '0';
+    }
+})
+
+let one = document.querySelector('#one');
+one.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '1';
+    } else {
+        displayValue.textContent += '1';
+    }
+})
+
+let two = document.querySelector('#two');
+two.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '2';
+    } else {
+        displayValue.textContent += '2';
+    }
+})
+
+let three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '3';
+    } else {
+        displayValue.textContent += '3';
+    }
+})
+
+let four = document.querySelector('#four');
+four.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '4';
+    } else {
+        displayValue.textContent += '4';
+    }
+})
+
+let five = document.querySelector('#five');
+five.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '5';
+    } else {
+        displayValue.textContent += '5';
+    }
+})
+
+let six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '6';
+    } else {
+        displayValue.textContent += '6';
+    }
+})
+
+let seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '7';
+    } else {
+        displayValue.textContent += '7';
+    }
+})
+let eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '8';
+    } else {
+        displayValue.textContent += '8';
+    }
+})
+
+let nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    if (displayValue.textContent == '0') {
+        displayValue.textContent = '9';
+    } else {
+        displayValue.textContent += '9';
+    }
+})
+
+
 function add(a, b) {
     let addSum = a + b;
     return addSum;
